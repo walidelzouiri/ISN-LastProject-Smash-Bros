dee4d49a81828812b03f83fd46d5df0980537064
BuildTools <unconfigured@null.spigotmc.org>
2018-02-04 20:08:32 +0100

    Fix
    Escape press == Game end

--- Brawl.py
+++ Brawl.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-*
 import pygame
 
+from sys import exit
 pygame.init()
 
 fenetre = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN, 32)
 pygame.display.set_caption("Brawl")
 
+def gererClavierEtSouris():
+    global continuer
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = 0
+
+        Press = pygame.key.get_pressed()
+        if Press[pygame.K_ESCAPE] == True:
+            continuer = 0
 
 
 clock = pygame.time.Clock()
 continuer = 1
+
 while continuer == 1:
     clock.tick(60)
+    gererClavierEtSouris()
 
 pygame.quit()
+exit()
